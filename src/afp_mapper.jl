3e7c0e46f0483f8d3c319ba450aed8925a901484
ymh <hanyongming@gmail.com>
2020-12-19 14:37:50 -0500

    edited file path for gigaword include

--- src/afp_mapper.jl
+++ src/afp_mapper.jl
@@ -13,7 +13,7 @@ addprocs_slurm(6, exeflags="--project=$(Base.active_project())")
 @everywhere begin
     import Pkg; Pkg.instantiate()
 
-    include("gigaword_64k.jl")
+    include("/users/yh31/scratch/projects/gigaword_64k/src/gigaword_64k.jl")
     
     const path_afp_dir = "/users/yh31/scratch/datasets/entity_linking/raw_data/gigaword/giga5/data/afp_eng"
     const path_intermed_data = "/users/yh31/scratch/datasets/entity_linking/raw_data/gigaword/giga_topk/intermediate_files"
